ad8d6d3971e6d5e2b450006191d5b5b26e3e23eb
Khoroshilov Alexey <karnedg@mail.ru>
2021-06-07 11:55:52 +0300

    it works!

---
+++ arithmetic_1.py
@@ -0,0 +1,14 @@
+def arithmetic(arg1, arg2, op):
+    if op == '+':
+        return arg1 + arg2
+    elif op == '-':
+        return arg1 - arg2
+    elif op == '*':
+        return arg1 * arg2
+    elif op == '/':
+        return arg1 / arg2
+    else:
+        return "noname function"
+
+
+print(arithmetic(2, 2, '+'))
