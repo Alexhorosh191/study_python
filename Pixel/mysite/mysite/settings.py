41ae025963d2507a48a9c75ddb00228691e53a28
Khoroshilov Alexey <karnedg@mail.ru>
2021-09-14 17:55:29 +0300

    I finished 11 lesson Make html file, a little designed the page

--- Pixel/mysite/mysite/settings.py
+++ Pixel/mysite/mysite/settings.py
@@ -104,7 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+
+# LANGUAGE_CODE = 'en-us' this is the past
 
 TIME_ZONE = 'UTC'
 
